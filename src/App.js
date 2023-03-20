bb7076469131df6ca90699ee11066d8f96827498
Syvex <80794011+Syvex@users.noreply.github.com>
2023-03-20 12:50:57 +0100

    create mock data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
+  console.log();
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <label for="meeting-time">Choose a time for your appointment:</label>
+
+      <input
+        onChange={(e) => (e.preventDefault(), console.log(e.target.value))}
+        type="datetime-local"
+        id="meeting-time"
+        name="meeting-time"
+        value="2018-06-12T19:30"
+        min="2018-06-07T00:00"
+        max="2018-06-14T00:00"
+      />
     </div>
   );
 }
---
+++ src/mockData.js
@@ -0,0 +1,26 @@
+export const mockUsers = [
+  {
+    userId: 1,
+    firstName: 'Oli',
+    lastName: 'Wiese',
+    email: 'oliverwiese@email.de',
+  },
+];
+
+export const mockEvents = [
+  {
+    eventId: 1,
+    creatorId: 1,
+    title: 'Raidnight',
+    dateTime: '2023-03-21T19:30',
+    description: 'Fun gaming night with the boys.',
+  },
+];
+
+export const mockAttendees = [
+  {
+    attendeeId: 1,
+    eventId: 1,
+    userId: 1,
+  },
+];
