8a59b8a5afa1a6ed96576caa46c3617ff15ce0c9
Syvex <80794011+Syvex@users.noreply.github.com>
2023-03-20 23:27:26 +0100

    update styles, mockData and Overview page

--- src/mockData.js
+++ src/mockData.js
@@ -5,6 +5,90 @@ export const mockUsers = [
     lastName: 'Wiese',
     email: 'oliverwiese@email.de',
   },
+  {
+    userId: 2,
+    firstName: 'Anna',
+    lastName: 'Schmidt',
+    email: 'annaschmidt@email.de',
+  },
+  {
+    userId: 3,
+    firstName: 'Max',
+    lastName: 'Mustermann',
+    email: 'maxmustermann@email.de',
+  },
+  {
+    userId: 4,
+    firstName: 'Lisa',
+    lastName: 'Müller',
+    email: 'lisamueller@email.de',
+  },
+  {
+    userId: 5,
+    firstName: 'David',
+    lastName: 'Garcia',
+    email: 'davidgarcia@email.de',
+  },
+  {
+    userId: 6,
+    firstName: 'Sarah',
+    lastName: 'Lee',
+    email: 'sarahlee@email.de',
+  },
+  {
+    userId: 7,
+    firstName: 'Markus',
+    lastName: 'Bauer',
+    email: 'markusbauer@email.de',
+  },
+  {
+    userId: 8,
+    firstName: 'Julia',
+    lastName: 'Wolf',
+    email: 'juliawolf@email.de',
+  },
+  {
+    userId: 9,
+    firstName: 'Felix',
+    lastName: 'Maier',
+    email: 'felixmaier@email.de',
+  },
+  {
+    userId: 10,
+    firstName: 'Sophie',
+    lastName: 'Schulz',
+    email: 'sophieschulz@email.de',
+  },
+  {
+    userId: 11,
+    firstName: 'Johannes',
+    lastName: 'Krause',
+    email: 'johanneskrause@email.de',
+  },
+  {
+    userId: 12,
+    firstName: 'Mia',
+    lastName: 'Weber',
+    email: 'miaweber@email.de',
+  },
+  {
+    userId: 13,
+    firstName: 'Jonas',
+    lastName: 'Becker',
+    email: 'jonasbecker@email.de',
+  },
+  {
+    userId: 14,
+    firstName: 'Paul',
+    lastName: 'Hoffmann',
+    email: 'paulhoffmann@email.de',
+  },
+  {
+    userId: 15,
+    firstName: 'Marie',
+    lastName: 'Schwarz',
+    email: 'marieschwarz@email.de',
+  },
 ];
 
 export const mockEvents = [
@@ -15,6 +99,27 @@ export const mockEvents = [
     dateTime: '2023-03-21T19:30',
     description: 'Fun gaming night with the boys.',
   },
+  {
+    eventId: 2,
+    creatorId: 3,
+    title: 'Movie Night',
+    dateTime: '2023-04-10T20:00',
+    description: 'Watching latest releases in cinema.',
+  },
+  {
+    eventId: 3,
+    creatorId: 5,
+    title: 'Hiking Trip',
+    dateTime: '2023-05-15T08:00',
+    description: 'Exploring scenic mountain trails.',
+  },
+  {
+    eventId: 4,
+    creatorId: 9,
+    title: 'Board Game Night',
+    dateTime: '2023-06-02T18:00',
+    description: 'Playing classic and modern board games.',
+  },
 ];
 
 export const mockAttendees = [
@@ -22,5 +127,45 @@ export const mockAttendees = [
     attendeeId: 1,
     eventId: 1,
     userId: 1,
+  },
+  {
+    attendeeId: 2,
+    eventId: 1,
+    userId: 2,
+  },
+  {
+    attendeeId: 3,
+    eventId: 2,
+    userId: 2,
+  },
+  {
+    attendeeId: 4,
+    eventId: 2,
+    userId: 3,
+  },
+  {
+    attendeeId: 5,
+    eventId: 3,
+    userId: 3,
+  },
+  {
+    attendeeId: 6,
+    eventId: 3,
+    userId: 4,
+  },
+  {
+    attendeeId: 7,
+    eventId: 4,
+    userId: 1,
+  },
+  {
+    attendeeId: 8,
+    eventId: 4,
+    userId: 3,
+  },
+  {
+    attendeeId: 9,
+    eventId: 4,
+    userId: 5,
   },
 ];
