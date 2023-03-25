89e8d37c4cb1123f5db745ccf5f6642eed096761
Syvex <80794011+Syvex@users.noreply.github.com>
2023-03-25 03:48:34 +0100

    refactor project font

--- src/helpers.js
+++ src/helpers.js
@@ -15,7 +15,7 @@ export function formatDate(dateString) {
   const day = dateObj.getDate().toString().padStart(2, '0');
   const month = (dateObj.getMonth() + 1).toString().padStart(2, '0');
   const year = dateObj.getFullYear();
-  return `${day}-${month}-${year}`;
+  return `${day}.${month}.${year}`;
 }
 
 export function getDateAndTime(datetimeString) {
