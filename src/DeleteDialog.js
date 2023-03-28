a7813f8fead2f3acccd1660804bf79e439ca17cb
Syvex <80794011+Syvex@users.noreply.github.com>
2023-03-28 13:33:57 +0200

    fix height, improve overlay

--- src/DeleteDialog.js
+++ src/DeleteDialog.js
@@ -10,17 +10,19 @@ function EventDeleteDialog({ event, onDelete, openDialog, setOpenDialog }) {
   return (
     <>
       {openDialog && (
-        <div className="dialog">
-          <p>
-            Möchten Sie das Event [<b>{event.title}</b>] wirklich löschen?
-          </p>
-          <div className="dialog-btn-wrap">
-            <button id="delete-confirm" onClick={confirmDeletion}>
-              Ja, löschen
-            </button>
-            <button id="delete-cancel" onClick={() => setOpenDialog(false)}>
-              Abbrechen
-            </button>
+        <div className="overlay">
+          <div className="dialog">
+            <p>
+              Möchten Sie das Event [<b>{event.title}</b>] wirklich löschen?
+            </p>
+            <div className="dialog-btn-wrap">
+              <button id="delete-confirm" onClick={confirmDeletion}>
+                Ja, löschen
+              </button>
+              <button id="delete-cancel" onClick={() => setOpenDialog(false)}>
+                Abbrechen
+              </button>
+            </div>
           </div>
         </div>
       )}
